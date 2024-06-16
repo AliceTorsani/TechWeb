2e4fa43ca7d4a6b24f1c180d9d8eb7ca1ec62818
Alice Torsani <torsalice@gmail.com>
2024-06-16 19:23:21 +0200

    aggiunta funzione di filtraggio film

--- Cinema/Cinema/gestione/views.py
+++ Cinema/Cinema/gestione/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from .models import *
 
 # Create your views here.
 
@@ -7,7 +8,35 @@ def gestione_home(request):
     return render(request,template_name="gestione/home.html")
 
 class FilmListView(ListView):
-    pass
+    titolo = "Il nostro cinema offre attualmente "
+    model = Film
+    template_name = "gestione/lista_film.html"
+    context_object_name = 'object_list'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Recupera i parametri di filtro dalla richiesta GET
+        filter_category = self.request.GET.get('filter_category', None)
+        filter_value = self.request.GET.get('filter_value', None)
+
+        if filter_category and filter_value:
+            if filter_category == 'genere':
+                queryset = queryset.filter(genere__icontains=filter_value)
+            elif filter_category == 'extra':
+                if filter_value == 'in_3D':
+                    queryset = queryset.filter(in_3D=True)
+                elif filter_value == 'in_inglese':
+                    queryset = queryset.filter(in_inglese=True)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter_category'] = self.request.GET.get('filter_category', '')
+        context['filter_value'] = self.request.GET.get('filter_value', '')
+        return context
+
 
 def search(request):
     pass
